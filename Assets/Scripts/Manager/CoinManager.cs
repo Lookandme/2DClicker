aac938bb18a5fabfac9a3eb0914ce673a323db4e
Lookandme <kgg9386@gmail.com>
2024-11-14 16:55:45 +0900

    [Update] 코인 매니저 싱글톤화

--- Assets/Scripts/Manager/CoinManager.cs
+++ Assets/Scripts/Manager/CoinManager.cs
@@ -6,10 +6,19 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class CoinManager : MonoBehaviour
 {
+    private static CoinManager instance;
     [SerializeField] public TextMeshProUGUI score;
 
     public int coin;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
 
+        }
+        else Destroy(gameObject);
+    }
     private void Update()
     {
         score.text = $"Current Coin: {coin}"; // 점수가 일종의 재화 역할을 겸함 // 이 부분을 분리해야 할 부분
